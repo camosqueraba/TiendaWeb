5a326e37b0cfa21cfe3ae43ae46ccc1f9e512d06
Carlos Andres <Carlos Andres@CarlosAndresPC>
2020-12-29 22:32:11 -0500

    creando proyecto TiendaWeb

---
+++ TiendaWeb/src/java/modelo/Cliente.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author Carlos Andres
+ */
+public class Cliente {
+    
+    private int idCliente;
+    
+    
+    private String nombre;
+    private String cedula;
+    private String telefono;
+
+    public Cliente(String nombreCliente, String cedulaCliente, String telefono) {
+        this.nombre = nombreCliente;
+        this.cedula = cedulaCliente;
+        this.telefono = telefono;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombreCliente(String nombreCliente) {
+        this.nombre = nombreCliente;
+    }
+
+    public String getCedulaCliente() {
+        return cedula;
+    }
+
+    public void setCedula(String cedulaCliente) {
+        this.cedula = cedulaCliente;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    
+    
+    
+    
+}
